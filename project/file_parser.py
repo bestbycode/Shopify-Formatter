231028920f0e443e077fe19042ea239303032062
Muhammad Ali <ma5638@nyu.edu>
2023-03-22 17:03:11 +0400

    add some fixes

--- project/file_parser.py
+++ project/file_parser.py
@@ -7,22 +7,42 @@ def parse_excel_sheet(file, sheet_name=0, threshold=6):
     '''parses multiple tables from an excel sheet into multiple data frame objects. Returns [dfs, df_mds], where dfs is a list of data frames and df_mds their potential associated metadata'''
     xl = pd.ExcelFile(file)
     entire_sheet = xl.parse(sheet_name=sheet_name)
+    print("A")
+    print(entire_sheet)
+
+    # Return if there is only 1 sheet
+    # Check if there are any empty rows
+    ## If there is an empty row, followed by more data = more than 1 sheets
 
     # count the number of non-Nan cells in each row and then the change in that number between adjacent rows
     n_values = np.logical_not(entire_sheet.isnull()).sum(axis=1)
+
+    print("B")
+    print(n_values)
     n_values_deltas = n_values[1:] - n_values[:-1].values
+    print("C")
+    print(n_values_deltas)
 
     # define the beginnings and ends of tables using delta in n_values
     table_beginnings = n_values_deltas > threshold
+    # print("D")
+    # print(table_beginnings)
     table_beginnings = table_beginnings[table_beginnings].index
+    print("E")
+    print(table_beginnings)
     table_endings = n_values_deltas < -threshold
+    print("F")
+    print(table_endings)
     table_endings = table_endings[table_endings].index
+    print("G")
+    print(table_endings)
     if len(table_beginnings) < len(table_endings) or len(table_beginnings) > len(table_endings)+1:
         raise BaseException(
             'Could not detect equal number of beginnings and ends')
 
     # make data frames
     dfs = []
+    print("Hi", len(table_beginnings))
     for ind in range(len(table_beginnings)):
         start = table_beginnings[ind]+1
         if ind < len(table_endings):
@@ -31,7 +51,7 @@ def parse_excel_sheet(file, sheet_name=0, threshold=6):
             stop = entire_sheet.shape[0]
         df = xl.parse(sheet_name=sheet_name, skiprows=start, nrows=stop-start)
         dfs.append(df.drop([0], axis=0))
-    return pd.concat(dfs, axis=0).reset_index(drop=True)
+    return pd.concat(dfs, axis=0).reset_index(drop=True) if (len(dfs)!=0) else entire_sheet
 
 
 def read_json_file(file):
